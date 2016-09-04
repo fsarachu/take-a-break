7b4a7f576ef53dd8f4881725cd00ddc304aec79b
Franco Sarachu <fsarachu@gmail.com>
2016-09-04 17:46:57 -0300

    Added console output with time info

--- break_time.py
+++ break_time.py
@@ -1,11 +1,14 @@
-import webbrowser
 import time
+import webbrowser
 
 total_breaks = 3
 break_count = 0
 
+print "Program started at " + time.ctime()
+
 while break_count < total_breaks:
     time.sleep(5)
+    print "It's time to give it a break!" + time.ctime()
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
     break_count += 1
