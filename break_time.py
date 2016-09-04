f8b795e5b70584a32b6c70b8a79ba3c40e551380
Franco Sarachu <fsarachu@gmail.com>
2016-09-04 17:47:41 -0300

    Fix missing space

--- break_time.py
+++ break_time.py
@@ -8,7 +8,7 @@ print "Program started at " + time.ctime()
 
 while break_count < total_breaks:
     time.sleep(5)
-    print "It's time to give it a break!" + time.ctime()
+    print "It's time to give it a break! " + time.ctime()
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
     break_count += 1
